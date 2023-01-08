dab4ff25070f7344cfee70417178df9b59d86bc7
ramadan93 <116916317+ramadan93@users.noreply.github.com>
2023-01-08 21:16:13 +0200

    [ADD] console test dev branch

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
-console.log('hello from udapeople!')
+console.log('test dev branch !')
 
 async function bootstrap() {
   const logger = new AppLogger();
